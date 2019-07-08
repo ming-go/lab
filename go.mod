651f7f550af2c85672b27eebf215be517c80e29d
ming <apiming0369@gmail.com>
2019-07-08 19:25:24 +0800

    Add redis-lua-scripting lab

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ming-go/lab
+
+go 1.12
+
+require (
+	github.com/garyburd/redigo v1.6.0 // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
+	github.com/ming-go/pkg v0.0.0-20190708095421-58fefa0aeb48 // indirect
+)
---
+++ redis-lua-scripting/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/ming-go/pkg/mredigo"
+)
+
+const (
+	MREDIGO_KEY = "lua-test"
+)
+
+const (
+	LUA_SCRIPT_ECHO = `
+		return redis.call("ECHO", KEYS[1])
+	`
+)
+
+func main() {
+	cfg := mredigo.NewConfig()
+	cfg.Host = net.JoinHostPort("172.77.0.99", "6379")
+	cfg.Database = "7"
+
+	if err := mredigo.CreatePool(MREDIGO_KEY, true, cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	redisConn, err := mredigo.Connect(MREDIGO_KEY)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	luaScript := redis.NewScript(1, LUA_SCRIPT_ECHO)
+	s, err := redis.String(luaScript.Do(redisConn, "Hello, world!"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(s)
+
+}
