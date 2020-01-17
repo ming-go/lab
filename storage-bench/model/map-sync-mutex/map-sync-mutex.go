728c61fa2c60edcd9f1048e2f4092b47e9e62ec5
ming <apiming0369@gmail.com>
2020-01-17 18:28:14 +0800

    Add map-sync-mutex

--- storage-bench/model/map-sync-mutex/map-sync-mutex.go
+++ storage-bench/model/map-sync-mutex/map-sync-mutex.go
@@ -4,21 +4,29 @@ import "sync"
 
 type mapSyncMutex struct {
 	db  map[string][]byte
-	mux sync.Mutex
+	mux sync.RWMutex
 }
 
-func (msm *mapSyncMutex) Create(k string, v string) {
-
+func (msm *mapSyncMutex) Create(k string, v []byte) {
+	msm.mux.Lock()
+	defer msm.mux.Unlock()
+	msm.db[k] = v
 }
 
 func (msm *mapSyncMutex) Delete(k string) {
-
+	msm.mux.Lock()
+	defer msm.mux.Unlock()
+	delete(msm.db, k)
 }
 
-func (msm *mapSyncMutex) Update(k string, v []byte) []byte {
-	return []byte("")
+func (msm *mapSyncMutex) Update(k string, v []byte) {
+	msm.mux.Lock()
+	defer msm.mux.Unlock()
+	msm.db[k] = v
 }
 
-func (msm *mapSyncMutex) Retrieve(k string) {
-
+func (msm *mapSyncMutex) Retrieve(k string) []byte {
+	msm.mux.RLock()
+	defer msm.mux.RUnlock()
+	return msm.db[k]
 }
